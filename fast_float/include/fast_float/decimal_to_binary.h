7ee7d420d79e5931525d3cc8c9e21ad439d8e849
Daniel Lemire <lemire@gmail.com>
2020-09-17 15:50:48 -0400

    Added tweak.

--- fast_float/include/fast_float/decimal_to_binary.h
+++ fast_float/include/fast_float/decimal_to_binary.h
@@ -88,6 +88,43 @@ value128 compute_product(int64_t q, uint64_t w) {
 }
 
 
+// This will compute or rather approximate w * 5**q and return a pair of 64-bit words approximating
+// the results, with the "high" part corresponding to the most significant bits and the
+// low part corresponding to the least significant bits.
+// 
+// For small values of q, e.g., q in [0,27], the answer is always exact.
+//
+// Otherwise, we seek an answer that is exact but for only for the 
+// most significant  bit_precision bits.
+template <int bit_precision>
+fastfloat_really_inline
+value128 compute_product_approximation(int64_t q, uint64_t w) {
+  const int index = 2 * int(q - smallest_power_of_five);
+  // For small values of q, e.g., q in [0,27], the answer is always exact because
+  // The line value128 firstproduct = full_multiplication(w, power_of_five_128[index]);
+  // gives the exact answer. 
+  value128 firstproduct = full_multiplication(w, power_of_five_128[index]);
+  static_assert((bit_precision >= 0) && (bit_precision <= 64), " precision should  be in (0,64]");
+  constexpr uint64_t precision_mask = (bit_precision < 64) ? 
+               (uint64_t(0xFFFFFFFFFFFFFFFF) >> bit_precision) 
+               : uint64_t(0xFFFFFFFFFFFFFFFF);
+  if((firstproduct.high & precision_mask) == precision_mask) {
+    // regarding the second product, we only need secondproduct.high, but our expectation is that the compiler will optimize this extra work away if needed.
+    value128 secondproduct = full_multiplication(w, power_of_five_128[index + 1]);
+    firstproduct.low += secondproduct.high;
+    if(secondproduct.high > firstproduct.low) {
+    firstproduct.high++;
+    }
+    // At this point, we might need to add at most one to firstproduct, but this
+    // can only change the value of firstproduct.high if firstproduct.low is maximal.
+    if(firstproduct.low  == 0xFFFFFFFFFFFFFFFF) {
+      // This is very unlikely, but if so, we need to do much more work!
+      return complete_long_product(q,w);
+    }
+  }
+  return firstproduct;
+}
+
 namespace {
 /**
  * For q in (-400,350), we have that
@@ -129,7 +166,15 @@ fastfloat_really_inline
   int lz = leading_zeroes(w);
   w <<= lz;
 
-  value128 product = compute_product(q, w);
+  // The required precision is binary::mantissa_explicit_bits() + 3 because
+  // 1. We need the implicit bit
+  // 2. We need an extra bit for rounding purposes
+  // 3. We might lose a bit due to the "upperbit" routine (result too small, requiring a shift)
+  value128 product = compute_product_approximation<binary::mantissa_explicit_bits() + 3>(q, w);
+  // The "compute_product_approximation" function can be slightly slower than a branchless approach:
+  // value128 product = compute_product(q, w);
+  // but in practice, we can win big with the compute_product_approximation if its additional branch
+  // is easily predicted. Which is best is data specific.
   uint64_t upperbit = product.high >> 63;
   answer.mantissa = product.high >> (upperbit + 64 - binary::mantissa_explicit_bits() - 3);
   lz += int(1 ^ upperbit);
