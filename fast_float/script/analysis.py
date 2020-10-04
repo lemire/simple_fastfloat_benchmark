cecb1cfda7d8340c21cee5fc41b721631e643acb
Daniel Lemire <lemire@gmail.com>
2020-10-03 23:10:34 -0400

    Adding analysis.

---
+++ fast_float/script/analysis.py
@@ -0,0 +1,32 @@
+from math import floor
+
+def log2(x):
+   y = 0
+   while((1<<y) < x):
+     y = y + 1
+   return y
+
+maxval = 9999999999999999999*2**64
+
+for q in range(1,28):
+    d = 5**q
+    b = 127 + log2(d)
+    t = 2** b
+    N = 2** 128 - 2**64 - 1
+    c = t//d + 1
+    assert c < 2**128
+    assert c >= 2**127
+    K = N - (N%d)
+    #print(K)
+    #print( c * K * d<=( K + 1) * t)
+    if(not(c * K * d<=( K + 1) * t)):
+      #print(q)
+      #print (d * c * K < t * (K + 1) )
+      top = floor(t/(c  * d - t))
+      if(maxval > top):
+         print(hex(floor(t/(c  * d - t))), hex(N))
+
+
+   #c  * d /t<=1-1/K
+   #1/K<=1-c  * d /t
+   #K>=1/(1-c  * d /t)
