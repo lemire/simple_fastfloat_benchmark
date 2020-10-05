9586753f4f6762e9772a4f34671491bc81e80797
Daniel Lemire <lemire@gmail.com>
2020-10-05 14:53:58 -0400

    Saving.

--- fast_float/script/analysis.py
+++ fast_float/script/analysis.py
@@ -1,30 +1,23 @@
 from math import floor
 
 def log2(x):
+   """returns ceil(log2(x)))"""
    y = 0
    while((1<<y) < x):
      y = y + 1
    return y
 
-maxval = 9999999999999999999*2**64
 
-for q in range(1,1000):
+for q in range(1,17+1):
     d = 5**q
     b = 127 + log2(d)
     t = 2** b
-    N = maxval
     c = t//d + 1
     assert c < 2**128
     assert c >= 2**127
-    K = N - (N%d)
+    K = 2**127
     if(not(c * K * d<=( K + 1) * t)):
       print(q)
-      #print (d * c * K < t * (K + 1) )
       top = floor(t/(c  * d - t))
-      if(maxval > top):
-         print(hex(floor(t/(c  * d - t))), hex(N))
-
-
-   #c  * d /t<=1-1/K
-   #1/K<=1-c  * d /t
-   #K>=1/(1-c  * d /t)
+      sys.exit(-1)
+print("all good")
