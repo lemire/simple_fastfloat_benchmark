9e888eff393461591825aa61e34fb8fc0f8efbf1
Daniel Lemire <lemire@gmail.com>
2020-10-05 20:44:06 -0400

    Saving.

--- fast_float/script/analysis.py
+++ fast_float/script/analysis.py
@@ -26,6 +26,7 @@ for q in range(18, 344+1):
     b = 64 + 2*log2(d)
     t = 2**b
     c = t//d + 1
+    assert c > 2**(64 +log2(d))
     K = 2**64
     if(not(c * K * d<=( K + 1) * t)):
       print(q)
