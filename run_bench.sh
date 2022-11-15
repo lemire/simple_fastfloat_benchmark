dfb985fa7e435c4cc76d5f9e66f3a04e57e1c757
Daniel Lemire <daniel@lemire.me>
2022-11-15 11:01:34 -0500

    Adding canada_short

--- run_bench.sh
+++ run_bench.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-declare -a StringArray=("data/canada.txt" "data/mesh.txt" "-m uniform" "-m uniform -c" "-m simple_uniform32" "-m simple_uniform32 -c" "-m simple_int32" )
+declare -a StringArray=("-f data/canada.txt" "-f data/canada_short.txt" "-f data/mesh.txt" "-m uniform" "-m uniform -c" "-m simple_uniform32" "-m simple_uniform32 -c" "-m simple_int32" )
 
 #cmake -B build --parallel
 #cmake --build build --parallel
 for ((i = 0; i < ${#StringArray[@]}; i++))
 do
     echo "${StringArray[$i]}"
-   ./build/benchmarks/benchmark ${StringArray[$i]}  
+   ./build/benchmarks/benchmark ${StringArray[$i]}
 done
