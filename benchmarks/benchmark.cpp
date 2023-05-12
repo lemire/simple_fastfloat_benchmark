d1ca20f616c0467cf93fa17061e7a0ce0a5dccee
Maya Warrier <maya.warrierm@gmail.com>
2023-05-12 07:29:39 -0400

    Add Cygwin support

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -1,6 +1,11 @@
+#ifdef __CYGWIN__
+#define _GNU_SOURCE // for strtod_l
+#endif
 
+#ifndef __CYGWIN__
 #include "absl/strings/charconv.h"
 #include "absl/strings/numbers.h"
+#endif
 #include "fast_float/fast_float.h"
 
 #ifdef ENABLE_RYU
@@ -50,8 +55,8 @@
 #include <xlocale.h> // old glibc
 #endif
 #else            // not glibc
-#ifndef _MSC_VER // assume that everything that is not GLIBC and not Visual
-                 // Studio needs xlocale.h
+#if !defined(_MSC_VER) && !defined(__CYGWIN__) // assume that everything that is not GLIBC, Cygwin or Visual
+                                               // Studio needs xlocale.h
 #include <xlocale.h>
 #endif
 #endif
@@ -184,7 +189,7 @@ double findmax_fastfloat(std::vector<std::basic_string<CharT>> &s) {
   return answer;
 }
 
-
+#ifndef __CYGWIN__
 double findmax_absl_from_chars(std::vector<std::string> &s) {
   double answer = 0;
   double x = 0;
@@ -197,6 +202,8 @@ double findmax_absl_from_chars(std::vector<std::string> &s) {
   }
   return answer;
 }
+#endif
+
 #ifdef USING_COUNTERS
 template <class T, class CharT>
 std::vector<event_count> time_it_ns(std::vector<std::basic_string<CharT>> &lines,
@@ -334,7 +341,9 @@ void process(std::vector<std::string> &lines, size_t volume) {
 #ifdef ENABLE_RYU
   pretty_print(volume, lines.size(), "ryu_parse", time_it_ns(lines, findmax_ryus2d, repeat));
 #endif
+#ifndef __CYGWIN__
   pretty_print(volume, lines.size(), "abseil", time_it_ns(lines, findmax_absl_from_chars, repeat));
+#endif
   pretty_print(volume, lines.size(), "fastfloat", time_it_ns(lines, findmax_fastfloat<char>, repeat));
 #ifdef FROM_CHARS_AVAILABLE_MAYBE
   pretty_print(volume, lines.size(), "from_chars", time_it_ns(lines, findmax_from_chars, repeat));
