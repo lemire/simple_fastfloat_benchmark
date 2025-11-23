05375a922a4e17bd99c1025df1161da5a5ad70a7
Daniel Lemire <daniel@lemire.me>
2025-11-23 16:51:16 -0500

    removing double conversion

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -13,9 +13,6 @@
 #endif
 
 
-#include "double-conversion/ieee.h"
-#include "double-conversion/double-conversion.h"
-
 #define IEEE_8087
 #include "cxxopts.hpp"
 #if defined(__linux__) || (__APPLE__ &&  __aarch64__)
@@ -61,33 +58,6 @@
 #endif
 #endif
 
-template <typename CharT>
-double findmax_doubleconversion(std::vector<std::basic_string<CharT>> &s) {
-  double answer = 0;
-  double x;
-  // from_chars does not allow leading spaces:
-  // double_conversion::StringToDoubleConverter::ALLOW_LEADING_SPACES |
-  int flags = double_conversion::StringToDoubleConverter::ALLOW_TRAILING_JUNK |
-              double_conversion::StringToDoubleConverter::ALLOW_TRAILING_SPACES;
-  double empty_string_value = 0.0;
-  uc16 separator = double_conversion::StringToDoubleConverter::kNoSeparator;
-  double_conversion::StringToDoubleConverter converter(
-      flags, empty_string_value, double_conversion::Double::NaN(), NULL, NULL,
-      separator);
-  int processed_characters_count;
-  for (auto &st : s) {
-    if constexpr (std::is_same<CharT, char16_t>::value) {
-      x = converter.StringToDouble((const uc16*)st.data(), st.size(), &processed_characters_count);
-    } else { 
-      x = converter.StringToDouble(st.data(), st.size(), &processed_characters_count);
-    }
-    if (processed_characters_count == 0) {
-      throw std::runtime_error("bug in findmax_doubleconversion");
-    }
-    answer = answer > x ? answer : x;
-  }
-  return answer;
-}
 #ifdef _WIN32
 double findmax_strtod_16(std::vector<std::u16string>& s) {
   double answer = 0;
@@ -353,7 +323,6 @@ void process(std::vector<std::string> &lines, size_t volume) {
   double volumeMB = volume / (1024. * 1024.);
   std::cout << "ASCII volume = " << volumeMB << " MB " << std::endl;
   pretty_print(volume, lines.size(), "netlib", time_it_ns(lines, findmax_netlib, repeat));
-  pretty_print(volume, lines.size(), "doubleconversion", time_it_ns(lines, findmax_doubleconversion<char>, repeat));
   pretty_print(volume, lines.size(), "strtod", time_it_ns(lines, findmax_strtod, repeat));
 #ifdef ENABLE_RYU
   pretty_print(volume, lines.size(), "ryu_parse", time_it_ns(lines, findmax_ryus2d, repeat));
@@ -369,7 +338,6 @@ void process(std::vector<std::string> &lines, size_t volume) {
   volume = 2 * volume;
   volumeMB = volume / (1024. * 1024.);
   std::cout << "UTF-16 volume = " << volumeMB << " MB " << std::endl;
-  pretty_print(volume, lines.size(), "doubleconversion", time_it_ns(lines16, findmax_doubleconversion<char16_t>, repeat));
 #ifdef _WIN32
   pretty_print(volume, lines.size(), "wcstod", time_it_ns(lines16, findmax_strtod_16, repeat));
 #endif
